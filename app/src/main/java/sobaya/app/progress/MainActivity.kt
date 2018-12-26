fffb2f7728e288adde928c490d4e5b847e8bc411
sobaya <45986582+sobaya-0141@users.noreply.github.com>
2018-12-26 22:07:23 +0900

    初回コミット

---
+++ app/src/main/java/sobaya/app/progress/MainActivity.kt
@@ -0,0 +1,12 @@
+package sobaya.app.progress
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
